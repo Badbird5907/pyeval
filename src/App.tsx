bb52c858c287fd34e3ef6bc8658aa663e3e7f647
Badbird5907 <50347938+Badbird5907@users.noreply.github.com>
2023-03-29 15:37:45 -0400

    Add code highlighting & fix some keyboard issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, {useRef, useState} from 'react'
 import Keyboard from 'react-simple-keyboard'
 import "react-simple-keyboard/build/css/index.css";
+import CodeEditor from '@uiw/react-textarea-code-editor';
 
 function App() { // god awful code, but it works lmao
     const [layoutName, setLayoutName] = useState("default");
@@ -20,9 +21,14 @@ function App() { // god awful code, but it works lmao
         return await runPython(code);
     }
     const onChange = (input: string) => {
+        /*
         setInput(input);
         onChangeInput({target: {value: input}})
+        // call the keyup event
+        // @ts-ignore
+        // document.getElementById("output").dispatchEvent(new KeyboardEvent("keyup", {key: input}));
         console.log("Input changed", input);
+         */
     };
 
     const onKeyPress = (button: string) => {
@@ -31,7 +37,24 @@ function App() { // god awful code, but it works lmao
         /**
          * If you want to handle the shift and caps lock buttons
          */
-        if (button === "{shift}" || button === "{lock}") handleShift();
+        if (button === "{shift}" || button === "{lock}") {
+            handleShift();
+            return;
+        }
+        if (button === "{enter}") {
+            setInput(input + "\n");
+            return;
+        }
+        if (button === "{tab}") {
+            setInput(input + "  ");
+            return;
+        }
+        if (button === "{bksp}") {
+            setInput(input.slice(0, -1));
+            return;
+        }
+
+        setInput(input + button);
     };
 
     const handleShift = () => {
@@ -55,6 +78,7 @@ function App() { // god awful code, but it works lmao
             height: "100vh",
             width: "100vw",
         }}>
+            {/*
             <textarea
                 rows={20}
                 style={{
@@ -64,10 +88,25 @@ function App() { // god awful code, but it works lmao
                 placeholder={"Enter python code here..."}
                 onChange={onChangeInput}
             />
+            */}
+            <p>Note: Tab = 2 spaces</p>
+            <CodeEditor
+                value={input}
+                language="python"
+                placeholder="Please enter code."
+                onChange={(evn) => onChangeInput(evn)}
+                minHeight={20}
+                style={{
+                    fontSize: 12,
+                    width: "90vw",
+                    height: "40vh",
+                    backgroundColor: "#f5f5f5",
+                    fontFamily: 'ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace',
+                }}
+            />
             <Keyboard
                 keyboardRef={r => (keyboard.current = r)}
                 layoutName={layoutName}
-                onChange={onChange}
                 onKeyPress={onKeyPress}
             />
             <div>
