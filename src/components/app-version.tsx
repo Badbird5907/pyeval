4e47a2340ac8d45d46390113853c198c9cca74be
Badbird5907 <50347938+Badbird5907@users.noreply.github.com>
2024-03-20 13:51:06 -0400

    wtf

--- src/components/app-version.tsx
+++ src/components/app-version.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 const AppVersion = () => {
   const env = import.meta.env;
+  console.log(env)
   return (
     <div style={{
       // float on bottom left
@@ -12,7 +13,7 @@ const AppVersion = () => {
       color: 'gray',
     }}>
       {env.VITE_VERCEL_ENV ?
-          <a href={`https://github.com/${env.VERCEL_GIT_REPO_OWNER}/${env.VERCEL_GIT_REPO_OWNER}/commit/${env.VERCEL_GIT_COMMIT_SHA}`}>{env.VITE_VERCEL_ENV}:vercel:{env.VERCEL_GIT_PROVIDER}:{env.VERCEL_GIT_COMMIT_REF}/{env.VERCEL_GIT_COMMIT_SHA.substring(0, 7)}</a>
+          <a href={`https://github.com/${env.VERCEL_GIT_REPO_OWNER}/${env.VERCEL_GIT_REPO_OWNER}/commit/${env.VERCEL_GIT_COMMIT_SHA}`}>{env.VITE_VERCEL_ENV}:vercel:{env.VERCEL_GIT_PROVIDER}:{env.VERCEL_GIT_COMMIT_REF}/{env.VERCEL_GIT_COMMIT_SHA.toString().substring(0, 7)}</a>
         : <span>Development</span>
       }
     </div>
