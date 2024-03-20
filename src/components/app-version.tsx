cce8bb7db1e6396affbe32dcabfdd567d7a22810
Badbird5907 <50347938+Badbird5907@users.noreply.github.com>
2024-03-20 13:58:41 -0400

    remove link stuff

--- src/components/app-version.tsx
+++ src/components/app-version.tsx
@@ -15,6 +15,7 @@ const AppVersion = () => {
       {env.VITE_VERCEL_ENV ?
           <a style={{
             color: 'gray',
+            textDecoration: 'none',
           }}
              href={`https://github.com/${env.VITE_VERCEL_GIT_REPO_OWNER}/${env.VITE_VERCEL_GIT_REPO_SLUG}/commit/${env.VITE_VERCEL_GIT_COMMIT_SHA}`}>
             {env.VITE_VERCEL_ENV}:vercel:{env.VITE_VERCEL_GIT_PROVIDER}:{env.VITE_VERCEL_GIT_COMMIT_REF}/{env.VITE_VERCEL_GIT_COMMIT_SHA.toString().substring(0, 7)}
