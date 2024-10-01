d1cb90b6c416d01c2f5631c146287ad9bc12897a
Badbird5907 <50347938+Badbird5907@users.noreply.github.com>
2024-10-01 01:18:00 -0400

    feat: catch ctrl s

--- src/components/saves/save/button.tsx
+++ src/components/saves/save/button.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { useSaves } from "@/lib/saves";
 import { FaCheck, FaSave } from "react-icons/fa";
-import React from "react";
+import React, { useCallback, useEffect } from "react";
 import SaveNewDialog from "@/components/saves/save/save-new-dialog";
 import { useAppState } from "@/App";
 
@@ -10,29 +10,43 @@ const SaveButton = () => {
   const [dialogOpen, setDialogOpen] = React.useState(false);
   const [saveCheck, setSaveCheck] = React.useState(false);
   const [saveBounce, setSaveBounce] = React.useState(false);
+  const save = useCallback(() => {
+    const current = saves.getCurrentSave();
+    if (!current) {
+      setDialogOpen(true);
+    } else {
+      saves.save(current.id, useAppState.getState().input);
+      setSaveBounce(true);
+      setTimeout(() => {
+        // jank but it works and looks good
+        setSaveBounce(false);
+        setSaveCheck(true);
+        setTimeout(() => {
+          setSaveCheck(false);
+        }, 1000);
+      }, 1000);
+    }
+  }, []);
+  useEffect(() => {
+    // catch ctrl + s
+    const keydown = (e: KeyboardEvent) => {
+      if (e.ctrlKey && e.key === "s") {
+        e.preventDefault();
+        save();
+      }
+    };
+    document.addEventListener("keydown", keydown);
+    return () => {
+      document.removeEventListener("keydown", keydown);
+    };
+  }, []);
   return (
     <>
       <SaveNewDialog dialogState={[dialogOpen, setDialogOpen]} />
       <Button
         round="left"
         variant="outline"
-        onClick={() => {
-          const current = saves.getCurrentSave();
-          if (!current) {
-            setDialogOpen(true);
-          } else {
-            saves.save(current.id, useAppState.getState().input);
-            setSaveBounce(true);
-            setTimeout(() => {
-              // jank but it works and looks good
-              setSaveBounce(false);
-              setSaveCheck(true);
-              setTimeout(() => {
-                setSaveCheck(false);
-              }, 1000);
-            }, 1000);
-          }
-        }}
+        onClick={save}
         disabled={saveBounce || saveCheck}
       >
         {saveBounce ? (
